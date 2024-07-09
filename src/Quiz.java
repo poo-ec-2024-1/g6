a7eb35d5fefba973215d80efe2c13ea1516f1318
mariadutra19 <156721876+mariadutra19@users.noreply.github.com>
2024-07-09 00:31:21 -0300

    Primeiro commit : estrutura básica

---
+++ src/Quiz.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Quiz {
+    private List<String> perguntas;
+    private List<String> respostas;
+
+    public Quiz(List<String> perguntas, List<String> respostas) {
+        this.perguntas = perguntas;
+        this.respostas = respostas;
+    }
+
+    public List<String> getPerguntas() {
+        return perguntas;
+    }
+
+    public List<String> getRespostas() {
+        return respostas;
+    }
+}
