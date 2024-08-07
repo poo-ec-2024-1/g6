2a9e86be57590f8eff5aebebe7be8d9499bf460d
Andreia0u0 <155964265+Andreia0u0@users.noreply.github.com>
2024-08-06 21:41:24 -0300

    Update PerfilViewController.java

--- Projeto_final/PerfilViewController.java
+++ Projeto_final/PerfilViewController.java
@@ -13,33 +13,61 @@ import java.sql.SQLException;
 import javafx.scene.control.Button;
 import java.io.IOException;
 
+
+/**
+ * Controlador para a visão do perfil do usuário.
+ * Responsável por apresentar os dados do perfil do usuário.
+ * Responsável pelo funcionamento dos botões no perfil.
+ */
 public class PerfilViewController {
+    //Label para a descrição
     @FXML
-    private Label descricaoLabel; // Label para a descrição
+    private Label descricaoLabel;
+    
+    //Label para o curso
     @FXML
-    private Label cursoLabel; // Label para o curso
+    private Label cursoLabel;
+    
+     //Label para o nome
     @FXML
-    private Label nomeLabel; // Label para o nome
+    private Label nomeLabel;
+    
+    //Label para o email
     @FXML
     private Label emailLabel;
+    
+    //Botão para iniciar o quiz
     @FXML
-    private Button quizButton;
+    private Button quizButton; 
+    
+    //Botão para acessar os grupos de estudo
     @FXML
     private Button gruposButton;
+
+    //Botão de voltar
     @FXML
     private Button voltarButton;
     
+    //Email do usuário logado
     private String currentUserEmail;
-    
+
+    //Botão do projeto
     @FXML
     private Button projetoButton;
-    
+        /**
+     * Método iniciado com a inicialização do fxml.
+     * Carrega o email e os dados do perfil
+     */
     @FXML
     private void initialize() {
          currentUserEmail= UserSession.getInstance().getEmail();
         carregarDadosDoUsuario();
     }
-
+    
+    /**
+     * Carrega os dados do usuário pelo banco de dados.
+     * Apresenta os dados nos labels.
+     */
     private void carregarDadosDoUsuario() {
         String nome = buscarNomeDoUsuario(currentUserEmail);
         String curso = buscarCursoDoUsuario(currentUserEmail);
@@ -51,6 +79,11 @@ public class PerfilViewController {
         emailLabel.setText(currentUserEmail);
     }
 
+    /**
+     * Busca o nome do usuário no banco de dados.
+     * Se encontra @return o nome do usuário. 
+     * Se não encontra @return uma mensagem de erro.
+     */
     private String buscarNomeDoUsuario(String email) {
         String sql = "SELECT nome FROM Usuarios WHERE email = ?";
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:Usuarios.db");
@@ -67,6 +100,11 @@ public class PerfilViewController {
         return "Usuário não encontrado";
     }
 
+    /**
+     * Busca o curso do usuário no banco de dados.
+     * Se encontra @return o curso do usuário.
+     * Se não encontra @return uma mensagem de erro.
+     */
     private String buscarCursoDoUsuario(String email) {
         String sql = "SELECT curso FROM Usuarios WHERE email = ?";
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:Usuarios.db");
@@ -83,6 +121,11 @@ public class PerfilViewController {
         return "Curso não encontrado";
     }
 
+    /**
+     * Busca a descrição do usuário no banco de dados.
+     * Se encontra @return a descrição do usuário.
+     * Se não encontra @return uma mensagem de erro.
+     */
     private String buscarDescricaoDoUsuario(String email) {
         String sql = "SELECT descricao FROM Usuarios WHERE email = ?";
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:Usuarios.db");
@@ -99,6 +142,10 @@ public class PerfilViewController {
         return "Descrição não encontrada";
     }
     
+    /**
+     * Controla o botão de seleção de quiz.
+     * Carrega a tela de seleção de quiz.
+     */
     @FXML
     private void handleSelecaoButtonAction() {
         try {
@@ -111,6 +158,10 @@ public class PerfilViewController {
         }
     }
     
+    /**
+     * Controla o botão de grupos de estudo.
+     * Carrega a tela de grupos de estudo.
+     */
     @FXML
     private void handleGruposButtonAction() {
         try {
@@ -122,7 +173,12 @@ public class PerfilViewController {
             e.printStackTrace();
         }
     }
+
     
+    /**
+     * Controla o botão de voltar.
+     * Carrega anterior novamente.
+     */
     @FXML
     private void handleVoltarButtonAction() {
         try {
@@ -134,7 +190,12 @@ public class PerfilViewController {
             e.printStackTrace();
         }
     }
-    
+
+
+    /**
+     * Controla o botão de grupos de estudo.
+     * Carrega a tela de grupos de estudo.
+     */
     @FXML
     private void handleProjetoButtonAction() {
         try {
